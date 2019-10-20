89d7eee9feb9567f133eaffb1a88cd7770b32e06
YanWeiFan <a349162727@qq.com>
2019-10-20 11:33:10 +0800

    删除.idea配置文件

--- client/src/com/simple/server/Main.java
+++ client/src/com/simple/server/Main.java
@@ -3,7 +3,6 @@ package com.simple.server;
 import java.io.*;
 import java.net.InetAddress;
 import java.net.Socket;
-import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
